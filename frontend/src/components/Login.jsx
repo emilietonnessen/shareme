0ee1292393f7db1ec184902d9538b0c731ff94a8
Emilie Tønnessen <emilie@tonnessen.com>
2023-01-17 08:19:33 +0100

    Setup assets and routes

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
