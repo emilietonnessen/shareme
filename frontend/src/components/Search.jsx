45b7ebf1a4fcc71dcf0b3467db2b066a70e9ae66
Emilie Tønnessen <emilie@tonnessen.com>
2023-01-27 10:56:47 +0100

    Complete search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-const Search = () => {
-  return <div>Search</div>;
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+import { client } from './../client';
+import { feedQuery, searchQuery } from './../utils/data';
+
+const Search = ({ searchTerm }) => {
+  // 🏡 Local state 🏡
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // 🎣 Hooks 🎣
+
+  useEffect(() => {
+    if (searchTerm) {
+      setLoading(true);
+
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {/* Loader */}
+      {loading && <Spinner message="Searching for pins" />}
+
+      {/* Pins */}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+
+      {/* No pins */}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl">No pins found</div>
+      )}
+    </div>
+  );
 };
 
 export default Search;
