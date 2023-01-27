1d70d6aefaeadbe75a2dcb7bf0a75123d418e0d9
Emilie Tønnessen <emilie@tonnessen.com>
2023-01-27 11:12:58 +0100

    Navigate user to login page if not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import Home from './container/Home';
 import Login from './components/Login';
+import { fetchUser } from './utils/fetchUser';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate('/login');
+  }, []);
+
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
       <Routes>
