0f6b6615a86686fa98304134c5fcd2622a1d82c4
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-04 23:41:35 +0100

    Delete "save" schema

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,7 +1,6 @@
 import comments from './comments'
 import pin from './pin'
 import postedBy from './postedBy'
-import save from './save'
 import user from './user'
 
-export const schemaTypes = [user, comments, pin, postedBy, save]
+export const schemaTypes = [user, comments, pin, postedBy]
