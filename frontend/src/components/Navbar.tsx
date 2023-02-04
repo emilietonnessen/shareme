197db6ab58014e21825daf46906031cd3be57423
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-04 23:47:55 +0100

    Move navigation from Home to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,46 +1,90 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { HiMenu } from 'react-icons/hi';
 import { IoMdAdd, IoMdSearch } from 'react-icons/io';
+import { Link, useNavigate } from 'react-router-dom';
+import { useMediaQuery } from 'usehooks-ts';
+
+import logo from '../assets/logo.png';
 import { UserProps } from './../utils/schemaTypes';
 
 interface NavbarProps {
   searchTerm: string;
   setSearchTerm: any;
   user: UserProps;
+  setToggleSidebar: (toggleSidebar: boolean) => void;
 }
 
-const Navbar = ({ searchTerm, setSearchTerm, user }: NavbarProps) => {
+const Navbar = ({
+  searchTerm,
+  setSearchTerm,
+  user,
+  setToggleSidebar,
+}: NavbarProps) => {
   // 🎣 Hooks 🎣
   const navigate = useNavigate();
+  const isMobile = useMediaQuery('(max-width: 768px)'); // 768px equals "md" in tailwind
 
-  if (!user) return null;
+  // If the user is not logged in
+  // if (!user) return null;
 
   return (
-    <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
-      <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
-        <IoMdSearch fontSize={21} className="ml-1" />
-        <input
-          type="text"
-          name="search"
-          id="search"
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search"
-          value={searchTerm}
-          onFocus={() => navigate('/search')}
-          className="p-2 w-full bg-white outline-none"
-        />
+    <header className="flex flex-col ">
+      {/* Mobile top bar */}
+      {isMobile && (
+        <div className="flex flex-row">
+          <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+            <HiMenu
+              fontSize={40}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(true)}
+            />
+            <Link to="/">
+              <img src={logo} alt="shareme logo" className="w-28" />
+            </Link>
+            <Link to={`user-profile/${user?._id}`}>
+              <img src={user?.image} alt="user" className="w-28" />
+            </Link>
+          </div>
+        </div>
+      )}
+
+      {/* Search bar */}
+      <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 p-2 md:p-5">
+        {/* Search input */}
+        <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
+          <button>
+            <IoMdSearch fontSize={21} className="ml-1" />
+            <span className="sr-only">Search pins</span>
+          </button>
+          <label htmlFor="search" className="text-sm -mt-14 absolute">
+            Search pins
+          </label>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Enter search term"
+            value={searchTerm}
+            onFocus={() => navigate('/search')}
+            className="p-2 w-full bg-white outline-none"
+          />
+        </div>
+
+        {/* User + Create Pin */}
+        <nav className="flex gap-3">
+          <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+            <img src={user.image} alt="user" className="w-14 h-12 rounded-lg" />
+          </Link>
+          <Link
+            to="/create-pin"
+            className="bg-black text-white rounded-lg w-12 md:w-14 md:h-12 flex justify-center items-center"
+          >
+            <span className="sr-only">Add pin</span>
+            <IoMdAdd />
+          </Link>
+        </nav>
       </div>
-      <div className="flex gap-3">
-        <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-          <img src={user.image} alt="user" className="w-14 h-12 rounded-lg" />
-        </Link>
-        <Link
-          to="/create-pin"
-          className="bg-black text-white rounded-lg w-12 md:w-14 md:h-12 flex justify-center items-center"
-        >
-          <IoMdAdd />
-        </Link>
-      </div>
-    </div>
+    </header>
   );
 };
 
--- frontend/src/container/Home.tsx
+++ frontend/src/container/Home.tsx
@@ -1,16 +1,13 @@
 import { AiFillCloseCircle } from 'react-icons/ai';
-import { HiMenu } from 'react-icons/hi';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { useState, useRef, useEffect } from 'react';
 
 import Pins from './Pins';
-import logo from '../assets/logo.png';
-import { client } from '../client';
-import { userQuery } from '../utils/data';
-import { fetchUser } from '../utils/fetchUser';
-import { UserProps } from './../utils/schemaTypes';
 import Sidebar from '../components/Sidebar';
-import UserProfile from '../components/UserProfile';
+import { UserProps } from './../utils/schemaTypes';
+import { client } from '../client';
+import { fetchUser } from '../utils/fetchUser';
+import { userQuery } from '../utils/data';
 
 const Home = () => {
   // 🎣 Hooks 🎣
@@ -38,23 +35,13 @@ const Home = () => {
 
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+      {/* Sidebar */}
       <div className="hidden md:flex h-screen flex-initial">
-        {user && <Sidebar user={user} />}
+        {user && <Sidebar />}
       </div>
+
+      {/* Mobile Sidebar */}
       <div className="flex md:hidden flex-row">
-        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
-          <HiMenu
-            fontSize={40}
-            className="cursor-pointer"
-            onClick={() => setToggleSidebar(true)}
-          />
-          <Link to="/">
-            <img src={logo} alt="logo" className="w-28" />
-          </Link>
-          <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="user" className="w-28" />
-          </Link>
-        </div>
         {toggleSidebar && (
           <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
             <div className="absolute w-full flex justify-end items-center p-2">
@@ -64,14 +51,18 @@ const Home = () => {
                 onClick={() => setToggleSidebar(false)}
               />
             </div>
-            {user && <Sidebar user={user} closeToggle={setToggleSidebar} />}
+            {user && <Sidebar closeToggle={setToggleSidebar} />}
           </div>
         )}
       </div>
       <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
         <Routes>
-          <Route path="/user-profile/:userId" element={<UserProfile />} />
-          {user && <Route path="/*" element={<Pins user={user} />} />}
+          {user && (
+            <Route
+              path="/*"
+              element={<Pins user={user} setToggleSidebar={setToggleSidebar} />}
+            />
+          )}
         </Routes>
       </div>
     </div>
