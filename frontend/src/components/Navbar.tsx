da3234001ef8675bb0ed83c602ea3026e28ce37f
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-07 09:10:47 +0100

    Redesign the look and behaviour of the navbar to function better on mobile devices

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,87 +1,62 @@
 import { HiMenu } from 'react-icons/hi';
-import { IoMdAdd, IoMdSearch } from 'react-icons/io';
-import { Link, useNavigate } from 'react-router-dom';
-import { useMediaQuery } from 'usehooks-ts';
+import { IoMdAdd } from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
-import logo from '../assets/logo.png';
+import SearchBar from './SearchInput';
 import { UserProps } from './../utils/schemaTypes';
 
 interface NavbarProps {
+  searchHandler: () => void;
   searchTerm: string;
   setSearchTerm: any;
-  user: UserProps;
   setToggleSidebar: (toggleSidebar: boolean) => void;
+  user: UserProps;
 }
 
 const Navbar = ({
+  searchHandler,
   searchTerm,
   setSearchTerm,
-  user,
   setToggleSidebar,
+  user,
 }: NavbarProps) => {
-  // 🎣 Hooks 🎣
-  const navigate = useNavigate();
-  const isMobile = useMediaQuery('(max-width: 768px)'); // 768px equals "md" in tailwind
-
-  // If the user is not logged in
-  // if (!user) return null;
-
   return (
     <header className="flex flex-col ">
-      {/* Mobile top bar */}
-      {isMobile && (
-        <div className="flex flex-row">
-          <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
-            <HiMenu
-              fontSize={40}
-              className="cursor-pointer"
-              onClick={() => setToggleSidebar(true)}
-            />
-            <Link to="/">
-              <img src={logo} alt="shareme logo" className="w-28" />
-            </Link>
-            <Link to={`user-profile/${user?._id}`}>
-              <img src={user?.image} alt="user" className="w-28" />
-            </Link>
-          </div>
-        </div>
-      )}
-
       {/* Search bar */}
-      <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 p-2 md:p-5">
+      <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 p-2 md:p-5 md:flex-row  items-end flex-col-reverse">
         {/* Search input */}
-        <div className="flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm">
-          <button>
-            <IoMdSearch fontSize={21} className="ml-1" />
-            <span className="sr-only">Search pins</span>
-          </button>
-          <label htmlFor="search" className="text-sm -mt-14 absolute">
-            Search pins
-          </label>
-          <input
-            type="text"
-            name="search"
-            id="search"
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Enter search term"
-            value={searchTerm}
-            onFocus={() => navigate('/search')}
-            className="p-2 w-full bg-white outline-none"
+        <div className="w-full">
+          <SearchBar
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            searchHandler={searchHandler}
           />
         </div>
 
-        {/* User + Create Pin */}
-        <nav className="flex gap-3">
-          <Link to={`user-profile/${user?._id}`} className="hidden md:block">
-            <img src={user.image} alt="user" className="w-14 h-12 rounded-lg" />
-          </Link>
-          <Link
-            to="/create-pin"
-            className="bg-black text-white rounded-lg w-12 md:w-14 md:h-12 flex justify-center items-center"
-          >
-            <span className="sr-only">Add pin</span>
-            <IoMdAdd />
-          </Link>
+        {/* Hamburger menu + User profile + Create Pin */}
+        <nav className="flex gap-3 justify-between w-full md:w-auto">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer md:hidden"
+            onClick={() => setToggleSidebar(true)}
+          />
+
+          <div className="flex gap-2">
+            <Link to={`user-profile/${user?._id}`} className="rounded-lg">
+              <img
+                src={user.image}
+                alt="user"
+                className="h-14 max-w-none rounded-lg"
+              />
+            </Link>
+            <Link
+              to="/create-pin"
+              className="bg-black text-white rounded-lg w-14 h-14 flex justify-center items-center"
+            >
+              <span className="sr-only">Add pin</span>
+              <IoMdAdd />
+            </Link>
+          </div>
         </nav>
       </div>
     </header>
