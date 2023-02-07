e6db0cc6be3b66ebc42a52d727911901cd2cc175
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-07 09:09:41 +0100

    Adjust the background value

--- frontend/src/container/Home.tsx
+++ frontend/src/container/Home.tsx
@@ -34,7 +34,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+    <div className="flex bg-gray-200 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
       {/* Sidebar */}
       <div className="hidden md:flex h-screen flex-initial">
         {user && <Sidebar />}
