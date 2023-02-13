fd7405471f6f7986dcad794649a29858b0c06209
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-13 12:17:19 +0100

    Create Comments.tsx

---
+++ frontend/src/components/Comments.tsx
@@ -0,0 +1,115 @@
+import DeleteButton from './DeleteButton';
+import ErrorBox from './ErrorBox';
+import SpinnerSmall from './SpinnerSmall';
+import UserProfileLink from './UserProfileLink';
+import { CommentsProps, PinProps } from '../utils/schemaTypes';
+
+interface CommentsComponentProps {
+  pinDetail: PinProps | null;
+  setInputValue: (value: string) => void;
+  addComment: () => void;
+  isAddingCommentLoading: boolean;
+  inputValue: string;
+  isAddingCommentError: boolean;
+  deleteComment: any;
+  userId: string;
+  comments: CommentsProps[];
+}
+
+const Comments = ({
+  pinDetail,
+  setInputValue,
+  addComment,
+  isAddingCommentLoading,
+  inputValue,
+  isAddingCommentError,
+  deleteComment,
+  userId,
+  comments,
+}: CommentsComponentProps) => {
+  return (
+    <div className="flex flex-col h-full justify-between ">
+      <div className="">
+        {/* Headline */}
+        <h2 className="text-3xl">Comments</h2>
+
+        {/* Comments */}
+        <ul className="">
+          {comments?.map((comment, i) => (
+            <li
+              key={i}
+              className="flex gap-2 justify-between mt-5 items-center bg-gray-100 p-4 rounded-lg"
+            >
+              <UserProfileLink
+                id={comment?.postedBy?._id || ''}
+                image={comment?.postedBy?.image || ''}
+                classesLink="flex"
+                classesImage="w-10 h-10 rounded-full cursor-pointer"
+                classesText="flex flex-col"
+              />
+              <div className="flex-1">
+                <p className="font-bold text-sm">
+                  {comment?.postedBy?.userName}
+                </p>
+                <p>{comment?.comment}</p>
+              </div>
+              {pinDetail?.postedBy?._id === userId && (
+                <DeleteButton
+                  screenReaderMessage={`Delete your "${comment?.comment}" comment`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteComment(comment?._key || '');
+                  }}
+                />
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Create comments */}
+      <form className="flex flex-wrap mt-6 gap-3 w-full justify-center items-end">
+        <img
+          src={pinDetail?.postedBy?.image}
+          alt=""
+          className="w-11 h-11 rounded-full"
+        />
+        <div className="flex flex-col flex-1">
+          <label htmlFor="comment" className="ml-2 text-sm font-bold">
+            Add comment
+          </label>
+          <input
+            type="text"
+            name="comment"
+            id="comment"
+            placeholder="What do you think of this image?"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="flex-1 border-gray-200 border-2 px-2 rounded-md block leading-10 placeholder:text-gray-500"
+          />
+        </div>
+        {isAddingCommentLoading ? (
+          <div className="h-11 flex items-center w-20 justify-center">
+            <SpinnerSmall />
+          </div>
+        ) : (
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              addComment();
+            }}
+            className="bg-salmon text-white rounded-md  py-2 font-semibold text-base h-11 w-20"
+          >
+            Post
+          </button>
+        )}
+        {isAddingCommentError && (
+          <ErrorBox message="Unable to save comment. Please try again later." />
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default Comments;
