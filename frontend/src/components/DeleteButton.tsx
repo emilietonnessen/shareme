8ae4eeedcc3bdbac92e1d89e8c6a4d6c30a36b2f
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-17 10:42:12 +0100

    Add loading spinner

--- frontend/src/components/DeleteButton.tsx
+++ frontend/src/components/DeleteButton.tsx
@@ -1,19 +1,31 @@
 import { FaTrashAlt } from 'react-icons/fa';
+import SpinnerSmall from './SpinnerSmall';
 
 interface DeleteButtonProps {
   onClick: (e: any) => void;
   screenReaderMessage: string;
+  loading: boolean;
 }
 
-const DeleteButton = ({ onClick, screenReaderMessage }: DeleteButtonProps) => {
+const DeleteButton = ({
+  onClick,
+  screenReaderMessage,
+  loading,
+}: DeleteButtonProps) => {
   return (
     <button
       type="button"
       className="w-14 h-14 hover:bg-gray-200 focus:bg-gray-200 text-salmon flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 font-bold text-base rounded-full"
       onClick={onClick}
     >
-      <FaTrashAlt fontSize={20} />
-      <span className="sr-only">{screenReaderMessage}</span>
+      {loading ? (
+        <SpinnerSmall />
+      ) : (
+        <>
+          <FaTrashAlt fontSize={20} />
+          <span className="sr-only">{screenReaderMessage}</span>
+        </>
+      )}
     </button>
   );
 };
