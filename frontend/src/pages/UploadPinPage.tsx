0e8e687b53c82bff8172caf764e4ae4b2e933f8d
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-20 15:25:31 +0100

    Divide the JSX into smaler components and increase limit for image size from 2mb to 20mb

--- frontend/src/pages/UploadPinPage.tsx
+++ frontend/src/pages/UploadPinPage.tsx
@@ -1,13 +1,16 @@
-import { AiOutlineCloudUpload } from 'react-icons/ai';
-import { MdDelete } from 'react-icons/md';
+import { SanityImageAssetDocument } from '@sanity/client';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
+import Dropdown from './../components/Inputs/Dropdown';
+import ErrorBox from './../components/NoticeBoxes/ErrorBox';
+import ImageUpload from './../containers/ImageUpload';
+import Input from '../components/Inputs/Input';
+import PrimaryButton from './../components/Buttons/PrimaryButton';
+import Textarea from '../components/Inputs/Textarea';
+import { UserProps } from '../utils/schemaTypes';
 import { categories } from '../utils/data';
 import { client } from '../client';
-import { UserProps } from '../utils/schemaTypes';
-import { SanityImageAssetDocument } from '@sanity/client';
-import Spinner from '../components/Spinners/Spinner';
 
 const UploadPinPage = ({ user }: { user: UserProps }) => {
   // 🏡 Local state 🏡
@@ -15,7 +18,7 @@ const UploadPinPage = ({ user }: { user: UserProps }) => {
   const [about, setAbout] = useState<string>('');
   const [destination, setDestination] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [fields, setFields] = useState<boolean>(false);
+  const [formError, setFormError] = useState<boolean>(false);
   const [category, setCategory] = useState<string>('');
   const [imageAsset, setImageAsset] = useState<SanityImageAssetDocument | null>(
     null
@@ -39,7 +42,7 @@ const UploadPinPage = ({ user }: { user: UserProps }) => {
       type === 'image/gif' ||
       type === 'image/tiff';
 
-    const isImageSizeCorrect = size <= 2000000;
+    const isImageSizeCorrect = size <= 20000000;
 
     // TODO: Add max 20MB file size
     if (isImageTypeCorrect) {
@@ -93,129 +96,68 @@ const UploadPinPage = ({ user }: { user: UserProps }) => {
         navigate('/');
       });
     } else {
-      setFields(true);
+      setFormError(true);
       // FIXME: I don't like handling errors like this. Add correct form validation instead
       setTimeout(() => {
-        setFields(false);
+        setFormError(false);
       }, 2000);
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-5 lg:h-4/5">
-      {fields && (
-        <p className="text-red-500 mb-5 text-xl transition-all duration-150 ease-in">
-          Please fill in all the fields.
-        </p>
-      )}
-      <div className="flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 max-w-6xl w-full rounded-md">
-        <div className="bg-secondaryColor p-3 flex flex-0.7 w-full">
-          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
-            {loading && <Spinner />}
-            {wrongImageType && (
-              <p>
-                Wrong image type. Please use either png, svg, jpeg, gif or tiff
-              </p>
-            )}
-            {wrongImageSize && (
-              <p>Wrong image size. Please use an image under 20MB</p>
-            )}
-            {!imageAsset ? (
-              <label>
-                <div className="flex flex-col items-center justify-center h-full">
-                  <div className="flex flex-col justify-center items-center">
-                    <p className="font-bold text-2xl">
-                      <AiOutlineCloudUpload />
-                    </p>
-                    <p className="text-lg">Click to upload</p>
-                  </div>
-                  <p className="mt-32 text-gray-400 text-center">
-                    Use high-quality JPEG, SVG, PNG, GIF or TIFF less than 2MB
-                  </p>
-                </div>
-                <input
-                  type="file"
-                  name="upload-image"
-                  id="upload-image"
-                  onChange={uploadImage}
-                  className="w-0 h-0"
-                />
-              </label>
-            ) : (
-              <div className="relative h-full">
-                <img
-                  src={imageAsset?.url}
-                  alt="uploaded-pic"
-                  className="h-full w-full"
-                />
-                <button
-                  className="absolute bottom-3 right-3 p-3 rounded-full bg-white text-xl cursor-pointer outline-none hover:shadow-md transition-all duration-500 ease-in-out"
-                  onClick={() => setImageAsset(null)}
-                >
-                  <MdDelete />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-1 flex-col gap-6 lg:pl-5 mt-5 w-full">
-          <input
-            type="text"
+    <div className="flex flex-col items-center mt-5 lg:h-4/5">
+      <div className="flex lg:flex-row flex-col gap-4 justify-center  bg-white lg:p-5 p-3 max-w-6xl w-full rounded-md">
+        {/* Image upload */}
+        <ImageUpload
+          imageAsset={imageAsset}
+          imageSizeError={wrongImageSize}
+          imageTypeError={wrongImageType}
+          loading={loading}
+          onClick={() => setImageAsset(null)}
+          uploadImage={uploadImage}
+        />
+
+        {/* Form */}
+        <form className="flex flex-1 flex-col gap-6 w-full pt-4 ">
+          <h1 className="text-3xl">Upload image</h1>
+          <Input
             value={title}
+            label="Title"
+            name="title"
             onChange={(e) => setTitle(e.target.value)}
             placeholder="Add your title"
-            className="outline-none text-2xl sm:text-3xl font-bold border-b-2 border-gray-200 p-2"
           />
-          <input
-            type="text"
-            value={about}
-            onChange={(e) => setAbout(e.target.value)}
-            placeholder="What is your pin about?"
-            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
-          />
-          <input
-            type="text"
+          <Input
+            label="Destination link"
+            name="destination"
             value={destination}
             onChange={(e) => setDestination(e.target.value)}
             placeholder="Add a destination link"
-            className="outline-none text-base sm:text-lg border-b-2 border-gray-200 p-2"
           />
-          <div className="flex flex-col">
-            <div>
-              <p className="mb-2 font-semibold text-lg sm:text-lg">
-                Choose Pin Category
-              </p>
-              <select
-                name=""
-                id=""
-                onChange={(e) => setCategory(e.target.value)}
-                className="w-full outline-none text-base border-b-2 border-gray-200 p-2 cursor-pointer"
-              >
-                <option value="other" className="vg.white">
-                  Select category
-                </option>
-                {categories.map((category) => (
-                  <option
-                    key={category.name}
-                    value={category.name}
-                    className="text-base border-0 outline-none capitalize bg-white text-black"
-                  >
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="flex justify-start items-end mt-5">
-              <button
-                className="bg-salmon text-white font-bold p-2 rounded-md w-full sm:w-28 outline-none"
-                type="button"
-                onClick={savePin}
-              >
-                Save Pin
-              </button>
-            </div>
-          </div>
-        </div>
+          <Textarea
+            label="About"
+            name="about"
+            value={about}
+            onChange={(e) => setAbout(e.target.value)}
+            placeholder="What is your pin about?"
+          />
+
+          <Dropdown
+            onChange={(e: any) => setCategory(e.target.value)}
+            options={categories}
+          />
+
+          {formError && <ErrorBox message="Please fill in all the fields" />}
+
+          <PrimaryButton
+            label="Upload image"
+            type="submit"
+            onClick={(e: any) => {
+              e.preventDefault();
+              savePin();
+            }}
+          />
+        </form>
       </div>
     </div>
   );
