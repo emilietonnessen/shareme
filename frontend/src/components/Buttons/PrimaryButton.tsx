f698b465041e0ab841bf5fe456e91925a27b5e8d
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-20 15:24:26 +0100

    Fill in empty component with button properties

--- frontend/src/components/Buttons/PrimaryButton.tsx
+++ frontend/src/components/Buttons/PrimaryButton.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+interface PrimaryButtonProps {
+  label: string;
+  onClick: any;
+  type?: 'button' | 'submit';
+}
 
-const PrimaryButton = () => {
-  return <div>PrimaryButton</div>;
+const PrimaryButton = ({
+  label,
+  onClick,
+  type = 'button',
+}: PrimaryButtonProps) => {
+  return (
+    <button
+      className="bg-salmon text-white font-bold p-2 md:px-4 rounded-md w-full sm:w-max outline-none"
+      type={type}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default PrimaryButton;
