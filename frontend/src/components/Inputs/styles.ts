35d3d7885c05ce03ccf7a19ef72e3634ef1febfa
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-20 15:23:30 +0100

    Create styles.ts

---
+++ frontend/src/components/Inputs/styles.ts
@@ -0,0 +1,3 @@
+export const labelStyles = 'text-salmon font-bold text-sm pb-1 pl-2';
+export const inputStyles =
+  'border-b-2 border-slate-300 p-2 placeholder:text-gray-500 placeholder:text-sm text-base bg-slate-100';
